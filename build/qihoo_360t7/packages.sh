70672ac9909f1ca48243d74ee1e66c6ffcfdfcaf
yunyuhao123aoiz <125276687+yunyuhao123aoiz@users.noreply.github.com>
2026-06-28 19:28:11 +0800

    Update packages.sh

--- build/qihoo_360t7/packages.sh
+++ build/qihoo_360t7/packages.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# 文件: build/qihoo_360t7/packages.sh
-# 设备专属官方源软件包
+# 360 T7 专属软件包 (官方源)
 
-export PACKAGES="$PACKAGES bc coreutils-flock conntrack nftables bash"
+export PACKAGES="$PACKAGES bc util-linux conntrack nftables bash"
