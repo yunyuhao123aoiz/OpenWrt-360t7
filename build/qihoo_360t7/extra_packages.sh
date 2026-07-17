44d6ee60e46d829f27a4e0210ad288c354e3db30
yunyuhao123aoiz <125276687+yunyuhao123aoiz@users.noreply.github.com>
2026-07-17 23:12:41 +0800

    Update extra_packages.sh

--- build/qihoo_360t7/extra_packages.sh
+++ build/qihoo_360t7/extra_packages.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # 只写包名，不带路径
-PACKAGES="$PACKAGES \
+export PACKAGES="$PACKAGES \
     luci-app-adguardhome \
     easytier \
     luci-app-easytier \
     luci-app-mosdns \
     luci-app-timecontrol \
-    luci-i18n-timecontrol-zh-cn \
-    luci-app-oaf \
-
+    luci-app-oaf"
