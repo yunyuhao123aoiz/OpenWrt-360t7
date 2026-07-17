81d9962c36640dfb83d13049b572455de03dcd7d
yunyuhao123aoiz <125276687+yunyuhao123aoiz@users.noreply.github.com>
2026-07-17 20:42:35 +0800

    Update config.sh

--- build/package/config.sh
+++ build/package/config.sh
@@ -1,3 +1,3 @@
 # build/package/config.sh
 # 列出您需要编译的包名（空格分隔）
-PACKAGES_TO_BUILD="luci-app-passwall luci-app-openclash my-custom-plugin"
+PACKAGES_TO_BUILD="luci-app-timecontrol luci-i18n-timecontrol-zh-cn"
